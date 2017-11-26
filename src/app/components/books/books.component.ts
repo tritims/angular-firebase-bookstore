423b308ea4694c75aaf4dd9f64ae8b5cfc7ec2f0
tritims <tritims@gmail.com>
2017-11-26 19:52:19 +0530

    Books list is being fetched from the database

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-books',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
-  constructor() { }
+  books: Observable<any[]>;
+  allBooks: any;
+
+  constructor(private db: AngularFireDatabase) { 
+    this.books = db.list('/books').valueChanges();
+    this.books.subscribe(books =>{
+      this.allBooks = books;
+    })
+  }
 
   ngOnInit() {
   }
